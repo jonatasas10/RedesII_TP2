c1b736502e558bdb87bffbaafc0abcfb528f075e
jonatasas10 <jonatasas17@gmail.com>
2024-02-13 13:36:57 -0300

    Experimento com sockets não bloqueantes

--- Project/cliente.py
+++ Project/cliente.py
@@ -1,7 +1,7 @@
-import socket
+import socket, select
 import os
 from dotenv import load_dotenv
-
+import pickle
 
 load_dotenv()
 
@@ -11,16 +11,45 @@ PORTA_TCP = int(os.environ.get("TCP_PORT"))
 PORTA_UDP = int(os.environ.get("UDP_PORT"))
 
 
-def receber_arquivo(nome_arquivo):
-    udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    udp_socket.bind(('0.0.0.0', PORTA_UDP))
-    dados, endereco_servidor = udp_socket.recvfrom(5242880)
+def receber_arquivo(udp_socket, nome_arquivo):
+    #udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    #udp_socket.bind(('0.0.0.0', PORTA_UDP))
+    pacote_final = []
+    proximo_numero_sequencia = 0
+
+    while True:
+
+        dados, endereco_servidor = udp_socket.recvfrom(102)
+        #print(endereco_servidor)
+        print(dados.decode('utf8'))
+        if dados.decode() == 'eof':
+            #udp_socket.sendto('eof'.encode(), endereco_servidor)
+            break
+
+        numero_seq, endereco_servidor = udp_socket.recvfrom(10)
+        print("\nTAMANHO DO PACOTE", len(dados), " / NUMERO SEQUENCIA", numero_seq.decode())
+        print("\n")
+        numero_seq = int(numero_seq.decode())
+        if proximo_numero_sequencia == 5:
+             udp_socket.sendto('5'.encode(), endereco_servidor)
+             udp_socket.sendto('5'.encode(), endereco_servidor)
+
+        if len(dados) > 50:
+            print("Prox:", numero_seq, proximo_numero_sequencia)
+            if numero_seq == proximo_numero_sequencia:
+                udp_socket.sendto(str(proximo_numero_sequencia).encode('utf8'), endereco_servidor)
+                proximo_numero_sequencia = proximo_numero_sequencia + 1
+
+            else:
+
+                udp_socket.sendto(str(proximo_numero_sequencia - 1).encode('utf8'), endereco_servidor)
+
 
     with open(nome_arquivo, 'wb') as arquivo:
         arquivo.write(dados)
         print(f"Arquivo {nome_arquivo} salvo com sucesso.")
         arquivo.close ()
-        udp_socket.close()
+        #udp_socket.close()
 
 
 # Função para lidar com as mensagens do servidor
@@ -32,45 +61,70 @@ def lidar_com_mensagens(cliente_socket):
     except ConnectionResetError:
         print("Conexão com o servidor foi encerrada.")
 
-# Criação do socket TCP do cliente
-cliente_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-cliente_socket.connect((HOST, PORTA_TCP))
 
-
-# Recebe a lista de arquivos do servidor
-arquivos_disponiveis = cliente_socket.recv(4096).decode()
 
 
 # Inicia a thread para lidar com mensagens do servidor
 # thread_mensagens = threading.Thread(target=lidar_com_mensagens, args=(cliente_socket,))
 # thread_mensagens.start()
 
+def main():
+    try:
+        # Criação do socket UDP do cliente
+        cliente_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        cliente_socket.sendto("start".encode(), (HOST, PORTA_UDP))
+        # Recebe a lista de arquivos do servidor # aqui estava dando erro logo aops recebimento, passou para cima.
+        arquivos_disponiveis = cliente_socket.recv(4096).decode()
+
+        nome_arquivo = "Tp02.txt"
+
+        cliente_socket.sendto(nome_arquivo.encode(), (HOST, PORTA_UDP))
+        receber_arquivo(cliente_socket, nome_arquivo)
+        print("recebido")
+
+    except Exception as e:
+        print(f"Erro: {e}")
+    finally:
+        cliente_socket.close()
+
+if __name__ == "__main__":
+    main()
+"""
 try:
 
+    # Criação do socket TCP do cliente
+    cliente_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    #cliente_socket.connect((HOST, PORTA_TCP))
+
+
     while True:
+        cliente_socket.sendto("start".encode(), (HOST, PORTA_UDP))
+        # Recebe a lista de arquivos do servidor
+        arquivos_disponiveis = cliente_socket.recv(4096).decode()
+        # Recebe a lista de arquivos do servidor # aqui estava dando erro logo aops recebimento, passou para cima.
+        # arquivos_disponiveis = cliente_socket.recv(4096).decode()  # o recv() nao precisa ser deste tamanho, pode ser menor '4096'.
 
-            # Recebe a lista de arquivos do servidor # aqui estava dando erro logo aops recebimento, passou para cima.
-            # arquivos_disponiveis = cliente_socket.recv(4096).decode()  # o recv() nao precisa ser deste tamanho, pode ser menor '4096'.
-        
-            print("Arquivos disponíveis para download:")
-            print(arquivos_disponiveis)
-        
-            # Solicitação do cliente para download usando UDP
-            nome_arquivo = input("Digite o nome do arquivo que deseja baixar (ou 'sair' para encerrar): ")
+        print("Arquivos disponíveis para download:")
+        print(arquivos_disponiveis)
 
-            if nome_arquivo.lower() == 'sair':
-                cliente_socket.sendall(nome_arquivo.encode())
-                # cliente_socket.close()
-                break
+        # Solicitação do cliente para download usando UDP
+        nome_arquivo = "Tp02.txt"#input("Digite o nome do arquivo que deseja baixar (ou 'sair' para encerrar): ")
 
-            cliente_socket.sendall(nome_arquivo.encode())
-            receber_arquivo(nome_arquivo)
-            
-           
+        if nome_arquivo.lower() == 'sair':
+            cliente_socket.sendto(nome_arquivo.encode(), (HOST, PORTA_UDP))
+            # cliente_socket.close()
+            break
+
+        cliente_socket.sendto(nome_arquivo.encode(), (HOST, PORTA_UDP))
+
+        #cliente_socket.sendto(nome_arquivo.encode(), (HOST, PORTA_UDP))
+        receber_arquivo(cliente_socket, nome_arquivo)
+        print("recebido")
+        cliente_socket.sendto("fim".encode(), (HOST, PORTA_UDP))
+
 except KeyboardInterrupt:
     pass
 
-
+"""
 # # Fecha a conexão TCP
-cliente_socket.close()
-
+#cliente_socket.close()
--- Project/servidor.py
+++ Project/servidor.py
@@ -1,8 +1,10 @@
-import socket
+import socket, select, traceback
 import os
 import threading
+import pickle
 from dotenv import load_dotenv
-
+from time import time
+import PyPDF2
 load_dotenv()
 dir = os.path.dirname(__file__)
 files_path = os.path.join(dir, os.environ.get("SERVER_FILES_PATH"))
@@ -10,71 +12,113 @@ HOST = os.environ.get("SERVER_HOST")
 PORTA_TCP = int(os.environ.get("TCP_PORT"))
 PORTA_UDP = int(os.environ.get("UDP_PORT"))
 
-# Função para listar os arquivos no diretório do servidor
-def listar_arquivos():
-    return os.listdir(files_path)
-
-# Função para enviar um arquivo para o cliente usando UDP
-def enviar_arquivo(nome_arquivo, endereco_cliente, porta_cliente):
-    try:
-        with open(os.path.join(files_path, nome_arquivo), 'rb') as arquivo:
-            cliente_udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-            # TODO: Adicionar metodo de envio aqui
-            while True:
-                dados = arquivo.read(1024)
-                if not dados:
-                    break
-                cliente_udp_socket.sendto(dados, (endereco_cliente, porta_cliente))
-                
-            cliente_udp_socket.close()
-            print(f"Enviado o arquivo {nome_arquivo} para {endereco_cliente}")
-    except FileNotFoundError:
-        print(f"Arquivo {nome_arquivo} não encontrado no servidor.")
-
-# Função para lidar com as solicitações de um cliente
-def lidar_com_cliente(conexao, endereco_cliente):
-    try:
-        # Envia a lista de arquivos para o cliente
-        arquivos_disponiveis = listar_arquivos()
-        mensagem_inicial = "\n".join(arquivos_disponiveis)
-        conexao.sendall(mensagem_inicial.encode())
-
-        # Loop para lidar com as solicitações do cliente
-        while True:
-            # Recebe a solicitação do cliente para download usando UDP
-            nome_arquivo = conexao.recv(4096).decode()
-
-            if not nome_arquivo:
-                break  # Cliente encerrou a conexão
-
-            # Envia o arquivo usando UDP
-            enviar_arquivo(nome_arquivo, endereco_cliente[0], PORTA_UDP)
-            
-    except ConnectionResetError:
-        print(f"Conexão com {endereco_cliente} foi encerrada pelo cliente.")
-
-    finally:
-        # Fecha a conexão TCP
-        conexao.close()
+ACK = 0
 
 # Configurações do servidor
 """ HOST = '127.0.0.1'
 PORTA_TCP = 12345
 PORTA_UDP = 54321 """
 
-# Criação do socket TCP do servidor
-servidor_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-servidor_socket.bind((HOST, PORTA_TCP))
-servidor_socket.listen()
+def receber_ack(servidor_socket):
+    global ACK
+    servidor_socket.setblocking(False)
+    ler_socket, _, _ = select.select([servidor_socket], [], [], 0.0)
+    for s in ler_socket:
+        if s is servidor_socket:
+            dados, _ = servidor_socket.recvfrom(100)
+            ACK = int(dados.decode('utf8'))
+            print("ACK recebido", ACK)
 
-print(f"Servidor ouvindo em {HOST}:{PORTA_TCP}")
-print(os.getcwd())
-while True:
-    # Aguarda por conexões
-    conexao, endereco_cliente = servidor_socket.accept()
-    print(f"Conexão estabelecida com {endereco_cliente}")
+# Função para listar os arquivos no diretório do servidor
+def listar_arquivos():
+    return os.listdir(files_path)
 
-    # Cria uma thread para lidar com o cliente
-    cliente_thread = threading.Thread(target=lidar_com_cliente, args=(conexao, endereco_cliente))
-    cliente_thread.start()
+# Função para enviar um arquivo para o cliente usando UDP
+def enviar_arquivo(nome_arquivo, endereco_cliente, servidor_socket):
+    try:
+        with open(os.path.join(files_path, nome_arquivo), 'rb') as arquivo:
+
+
+            global ACK
+            sequencia_base = 0
+            proximo_numero_sequencia = 0
+            arquivo.seek(sequencia_base)
+            N = 4
+            timeout = []
+            tempo = []
+
+            while True:
+                dados = arquivo.read(1024//10)
+
+                if not dados and ACK == proximo_numero_sequencia -1:
+                    print("Fim do arquivo, ACK =", ACK)
+
+                    break
+                if proximo_numero_sequencia < sequencia_base + N and dados:
+                    tempo.append(time())
+                    servidor_socket.sendto(dados, endereco_cliente)
+                    print("Número de sequência enviado:", proximo_numero_sequencia)
+                    servidor_socket.sendto(str(proximo_numero_sequencia).encode(), endereco_cliente)
+                    # Para receber um ack
+                    threading.Thread(target=receber_ack, args=[servidor_socket]).start()
+                    proximo_numero_sequencia += 1
+                else:
+                    servidor_socket.setblocking(True)
+                    dados_ack, _ = servidor_socket.recvfrom(100)
+                    print("Ultimos ACKS após enviar todo pacote:", dados_ack.decode())
+                    ACK = int(dados_ack.decode())
+
+                    #threading.Thread(target=receber_ack, args=[servidor_socket]).start()
+
+                sequencia_base += 1
+
+            servidor_socket.sendto('eof'.encode(), endereco_cliente)
+            print(f"Enviado o arquivo {nome_arquivo} para {endereco_cliente}")
+
+    except FileNotFoundError:
+        print(f"Arquivo {nome_arquivo} não encontrado no servidor.")
+    except Exception as e:
+        print("algum erro", e)
+        traceback.print_exc()
+
+# Função para lidar com as solicitações de um cliente
+def lidar_com_cliente(servidor_socket, endereco_cliente):
+
+    try:
+        # Envia a lista de arquivos para o cliente
+        arquivos_disponiveis = listar_arquivos()
+        mensagem_inicial = "\n".join(arquivos_disponiveis)
+
+        servidor_socket.sendto(mensagem_inicial.encode(), endereco_cliente)
+
+        nome_arquivo, _ = servidor_socket.recvfrom(1024)
+
+        nome_arquivo = nome_arquivo.decode()
+        print(nome_arquivo, endereco_cliente)
+        # Envia o arquivo usando UDP
+        enviar_arquivo(nome_arquivo, endereco_cliente, servidor_socket)
+
+    except ConnectionResetError:
+        print(f"Conexão com {endereco_cliente} foi encerrada pelo cliente.")
+
+    finally:
+        return nome_arquivo
+
+
+def main():
+    try:
+        servidor_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        servidor_socket.bind((HOST, PORTA_UDP))
+        while True:
+            #print(f"Servidor ouvindo em {HOST}:{PORTA_UDP}")
+            mensagem, endereco_cliente = servidor_socket.recvfrom(1024)
+            lidar_com_cliente(servidor_socket, endereco_cliente)
+
+    except KeyboardInterrupt:
+        print("\nEncerrando socket do servidor.")
+        servidor_socket.close()
+    finally:
+        servidor_socket.close()
+
+if __name__ == "__main__":
+    main()
